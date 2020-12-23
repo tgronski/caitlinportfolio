b0e6c316e69411cce25d976c6b571e4a37b4a3af
tgronski <tessgronski@gmail.com>
2020-12-23 12:59:16 -0600

    Edits width of articles, centers content

--- src/Article.js
+++ src/Article.js
@@ -9,18 +9,21 @@ class Article extends React.Component {
   let result=articles.filter(article=>article.id===id)[0]
   
   return (
-    <div className="Article">
+    <div className="Article-Content">
       <div>
+      <div className="Article-Byline-Title">
       <h4>{result.byline}</h4>
       <h1>{result.title}</h1>
+      </div>
       {(result.firstphoto)
-      ?(<><img className="Photo" src={result.firstphoto} alt="article-main"/>
-      <h5>{result.firstphotocredits}</h5></>)
+      ?(<div className="Content-Image"><img className="Photo" src={result.firstphoto} alt="article-main"/>
+      <h5>{result.firstphotocredits}</h5></div>)
       :(null)}
 
       <h3>{result.articlePart1}</h3>
+      <div className="Content-Image">
       <img className="Photo" src={result.secondphoto} alt="article-second" />
-      <h5>{result.secondphotocredits}</h5>
+      <h5>{result.secondphotocredits}</h5></div>
       <h3>{result.articlePart2}</h3>
       </div>
     </div>
